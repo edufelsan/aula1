9b05ec2422556e17495daefff9b8d729e41fab9e
Eduardo Felix <eduardo.felix@outlook.com>
2023-02-18 22:27:17 +0000

    Correções

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Drawing;
+
 namespace aula1
 {
     class Program
@@ -16,23 +18,53 @@ namespace aula1
             // O comando Console.WriteLine escreve uma linha no condole.
             //Console.WriteLine("Parabéns pelo seu primeiro código!");
 
-            int n1, n2, resultado;
-            Console.WriteLine("Digite o primeiro número");
-            n1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Digite o segundo número");
-            n2 = Convert.ToInt32(Console.ReadLine());
+            // int n1, n2, resultado;
+            // Console.WriteLine("Digite o primeiro número");
+            // n1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Digite o segundo número");
+            // n2 = Convert.ToInt32(Console.ReadLine());
 
-            resultado = n1 + n2;
-            Console.WriteLine("Soma = " + resultado);
+            // resultado = n1 + n2;
+            // Console.WriteLine("Soma = " + resultado);
             //Console.WriteLine("A soma = " + resultado.ToString()) ;
-            resultado = n1 - n2;
-            Console.WriteLine("Subtração  = " + resultado);
+            // resultado = n1 - n2;
+            // Console.WriteLine("Subtração  = " + resultado);
 
-            resultado = n1 * n2;
-            Console.WriteLine("Multiplicação = " + resultado);
+            //  resultado = n1 * n2;
+            //  Console.WriteLine("Multiplicação = " + resultado);
 
-            resultado = n1 / n2;
-            Console.WriteLine("Divisão = " + resultado);
+            //  resultado = n1 / n2;
+            //  Console.WriteLine("Divisão = " + resultado);
+
+            /*
+            double dolar, real, resultado;
+            Console.WriteLine("Digite quanto quer doar em dólar");
+            dolar = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a cotação do dolar hoje (em real)");
+            real = Convert.ToDouble(Console.ReadLine());
+
+            resultado = dolar * real;
+            Console.WriteLine("A sua doação será de = R$" + resultado);
+            */
+
+            /*
+            double km_inicial, km_final, litros, resultado;
+            Console.WriteLine("Qual a quilometragem inicial?");
+            km_inicial = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Qual a quilometragem final?");
+            km_final = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Quantos litros abastecidos?");
+            litros = Convert.ToDouble(Console.ReadLine());
+
+            resultado = (km_final - km_inicial) / litros;
+
+            Console.WriteLine("O carro faz " + resultado + "km por litro.");
+            */
+
+
 
         }
     }
